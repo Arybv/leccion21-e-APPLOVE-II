36aedc9ce4e16d0fba2a3440d43780f3807cb33e
Arantza Burga <arybv18@gmail.com>
2017-04-11 12:56:19 -0500

    leccion21-e-APPLOVE II.05

---
+++ assets/js/app04.js
@@ -0,0 +1,17 @@
+//modal
+var modal = document.getElementById("myModal");
+//imagenes
+var img = document.getElementById("myImg04");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+//span
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function(){
+  modal.style.display = "none";
+}
